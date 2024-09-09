c41d8a5289a3bd367ce652ed06dcee23f992144e
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-09 15:31:05 +0800

    feat: define the togglePassword func

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,13 @@ const eyeicon = document.querySelector("#eyeicon");
 const password = document.querySelector("#password");
 
 eyeicon.addEventListener("click", togglePassword);
+
+function togglePassword() {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon.src = "./images/eye-open.png";
+  } else {
+    password.type = "password";
+    eyeicon.src = "./images/eye-close.png";
+  }
+}
